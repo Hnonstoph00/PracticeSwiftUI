6ec2875f46421728e4a2468b1209b75b355ed4e4
hnostoph <hnonstoph@Hnonstophs-MacBook-Pro.local>
2024-04-06 12:59:39 +0700

    hanh_task_initilize_project

---
+++ PracticeSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PracticeSwiftUI
+//
+//  Created by Hnonstoph on 06/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by Hnonstoph on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
